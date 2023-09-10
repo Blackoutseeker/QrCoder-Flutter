f1472ab6a09d1900d7415f0664ac3eda2c878e47
Blackoutseeker <felipeoriginal.2019@gmail.com>
2023-09-09 23:25:51 -0300

    refactor(themes): Refactor using the const keyword where applicable

--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Dark {
-  final ThemeData theme = ThemeData(
+class DarkTheme {
+  static final ThemeData themeData = ThemeData(
     colorScheme: ColorScheme.fromSwatch().copyWith(
       secondary: const Color(0xFFC00C00),
     ),
     scaffoldBackgroundColor: const Color(0xFF121212),
     appBarTheme: const AppBarTheme(
-      backgroundColor: const Color(0xFF1E1E1E),
+      backgroundColor: Color(0xFF1E1E1E),
     ),
     inputDecorationTheme: const InputDecorationTheme(
-      hintStyle: const TextStyle(
-        color: const Color.fromRGBO(255, 255, 255, 0.5),
+      hintStyle: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.5),
       ),
-      fillColor: const Color.fromRGBO(255, 255, 255, 0.05),
+      fillColor: Color.fromRGBO(255, 255, 255, 0.05),
     ),
     dialogTheme: const DialogTheme(
-      backgroundColor: const Color(0xFF1E1E1E),
-      titleTextStyle: const TextStyle(
-        color: const Color.fromRGBO(255, 255, 255, 0.5),
+      backgroundColor: Color(0xFF1E1E1E),
+      titleTextStyle: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.5),
         fontSize: 18,
       ),
-      contentTextStyle: const TextStyle(
+      contentTextStyle: TextStyle(
         color: Colors.white,
         fontSize: 16,
       ),
